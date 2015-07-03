f9a42e2c8046aa574c6e0fba18c519052a344bf6
Larry Archer <larcher@spoon.cx>
2015-07-03 08:21:20 -0500

    Add an MpdPlaylist class (borrowed from the mpdserver example)

--- pianobar-mpd.py
+++ pianobar-mpd.py
@@ -42,8 +42,35 @@ class Next(mpdserver.Command):
     def handle_args(self):
         send_to_pianobar("n")
 
+# Define a MpdPlaylist based on mpdserver.MpdPlaylist
+# This class permits to generate adapted mpd respond on playlist command.
+class MpdPlaylist(mpdserver.MpdPlaylist):
+    playlist=[mpdserver.MpdPlaylistSong(file='file0',
+                                        title="Title of the song",
+                                        artist="Singer of the song",
+                                        album="Album of the song",
+                                        songId=0
+                                       )
+             ]
+    # How to get song position from a song id in your playlist
+    def songIdToPosition(self,i):
+        for e in self.playlist:
+            if e.id==i: 
+                return e.playlistPosition
+
+    # Set your playlist. It must be a list a MpdPlaylistSong
+    def handlePlaylist(self):
+        return self.playlist
+
+    # Move song in your playlist
+    def move(self,i,j):
+        self.playlist[i],self.playlist[j]=self.playlist[j],self.playlist[i]
+
 # Create a deamonized mpd server that listen on port 9999
 mpd = mpdserver.MpdServerDaemon(PORT)
+
+# Set the user defined playlist class
+mpd.requestHandler.Playlist = MpdPlaylist
 
 # Register provided outputs command 
 mpd.requestHandler.RegisterCommand(mpdserver.Outputs)
